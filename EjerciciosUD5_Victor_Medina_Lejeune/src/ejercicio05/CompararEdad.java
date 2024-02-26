82eb734b06077d9f5066cc5a51b483d459969799
Victor <victorml2204@gmail.com>
2024-02-26 13:41:18 +0100

    ns q pasa

--- EjerciciosUD5_Victor_Medina_Lejeune/src/ejercicio05/CompararEdad.java
+++ EjerciciosUD5_Victor_Medina_Lejeune/src/ejercicio05/CompararEdad.java
@@ -4,16 +4,16 @@ import java.util.Comparator;
 
 public class CompararEdad implements Comparator<Persona> {
 
-	public int compararPorEdad(Persona persona, Persona persona2) {
+	@Override
+	public int compare(Persona persona, Persona persona2) {
 		if (persona.getEdad() < persona2.getEdad()) {
 			return -1;
 		} else {
-			if (this.dorsal > o.dorsal) {
+			if (persona.getEdad() > persona2.getEdad()) {
 				return 1;
 			}
 		}
 		return 0;
 	}
 
-	}
 }
