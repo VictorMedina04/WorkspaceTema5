6d8dfa068bf3a03ffc129d601f6671b71b874bd0
Victor <medina.levic24@triana.salesianos.edu>
2024-03-07 12:17:46 +0100

    fd

---
+++ EjemplosUD5_Victor_Medina_Lejeune/src/ejemplosParte2/Principalfecha.java
@@ -0,0 +1,21 @@
+package ejemplosParte2;
+
+import java.time.LocalDate;
+
+public class Principalfecha {
+	public static void main(String[] args) {
+
+		LocalDate fechaHoy = LocalDate.now();
+
+		System.out.println(fechaHoy);
+
+		LocalDate fecha1 = LocalDate.of(2004, 4, 22);
+		System.out.println(fecha1);
+
+		LocalDate fecha = LocalDate.parse("2004-04-02");
+
+		System.out.println(fecha);
+
+	}
+
+}
